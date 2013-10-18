278aa21993a5f69c5bd45da072307683160c76d4
kellimohr <kellimohr@gmail.com>
2013-10-17 20:40:36 -0700

    Initial Commit

---
+++ HomeworkOne/KMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KMAppDelegate.h
+//  HomeworkOne
+//
+//  Created by Kelli Mohr on 10/17/13.
+//  Copyright (c) 2013 Kelli Mohr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HomeworkOne/KMViewController.h
@@ -0,0 +1,13 @@
+//
+//  KMViewController.h
+//  HomeworkOne
+//
+//  Created by Kelli Mohr on 10/17/13.
+//  Copyright (c) 2013 Kelli Mohr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMViewController : UIViewController
+
+@end
